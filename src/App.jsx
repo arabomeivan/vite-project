00bf606dd10de55171ea08324281db2fd917a8d2
Tegacreatives <contact@tegacreatives.com>
2024-08-21 10:55:28 +0100

    UI for sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { Button } from "flowbite-react";
-import './App.css'
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-
-
   return (
-    <>
-      <Button>Click me</Button>
-    </>
-  )
+    <main className="md:grid md:grid-cols-8 bg-[#F1F1F1]">
+      <div className="hidden md:block md:col-span-2">
+        <Sidebar />
+      </div>
+      <div className="col-span-6">
+        <Dashboard />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
